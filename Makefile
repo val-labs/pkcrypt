c67e689b25d1d5902f82b83a6b37ecc4944d2e71
Ward, Joel <jward@conversantmedia.com>
2018-03-13 13:38:22 -0700

    update

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
 all: clean unit end2end
 clean: ; @rm -fr x.* *.pyc
-unit:  ; @./test.py
+unit:  ; @PYTHONPATH=. ./pkcrypt/test.py
 end2end:
 	@python -um pkcrypt gensk -o       x.sk
 	@python -um pkcrypt genvk -o x.vk  x.sk
 	@python -um pkcrypt sign  -o x.sig x.sk < Makefile
 	@cat x.sig Makefile | python -um pkcrypt verify x.vk
+	@python -um pkcrypt gensk -no       x.sk2
+	@python -um pkcrypt genvk -no x.vk2  x.sk2
+	@python -um pkcrypt sign  -no x.sig2 x.sk2 < Makefile
---
+++ pkcrypt/__main__.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+from pkcrypt import *
+
+def cli(argv=sys.argv):
+    eol, fname = '\n', ''
+
+    def getarg2(name):
+        try:
+            if sys.argv[2] == name:
+                sys.argv.pop(2)
+                return 1
+        except:
+            return
+
+    def output(msg):
+        with open(fname,'w') if fname else sys.stdout as f:
+            f.write(msg + eol)
+
+    if getarg2('-no'): eol = ''; fname = sys.argv.pop(2)
+    if getarg2('-n'):  eol = ''
+    if getarg2('-o'):            fname = sys.argv.pop(2)
+
+    if   sys.argv[1] == 'gensk':
+        output(sk2str(gen_sk()))
+
+    elif sys.argv[1] == 'genvk':
+        output(vk2str(get_vk(fload_sk())))
+        
+    elif sys.argv[1] == 'sign':
+        output(sig2str(sign_with(fload_sk(), load_msg())))
+
+    elif sys.argv[1] == 'verify':
+        if invalid_sig(fload_vk(), load_sig(), load_msg()):
+            print("NOT VALID")
+            exit(1)
+      
+    else:
+        print("BAD ARGS")
+        raise exit(2)
+
+if __name__ == '__main__': cli()
