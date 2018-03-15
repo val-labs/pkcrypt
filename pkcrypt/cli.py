ff8833badad1362b490553afad6c054c713f172f
Ward, Joel <jward@conversantmedia.com>
2018-03-15 15:14:31 -0700

    update

--- pkcrypt/cli.py
+++ pkcrypt/cli.py
@@ -17,9 +17,10 @@ def cli(argv=sys.argv):
         except:
             return
 
-    def output(msg):
+    def output(msg, _eol = None):
+        _eol if _eol is not None else eol
         with open(fname,'w') if fname else sys.stdout as f:
-            f.write(msg + eol)
+            f.write(msg + _eol)
 
     if getarg2('-no'): eol = ''; fname = sys.argv.pop(2)
     if getarg2('-n'):  eol = ''
@@ -39,13 +40,23 @@ def cli(argv=sys.argv):
     elif arg1 == 'sign':
         msg = load_msg()
         sgn = sig2str(sign_with(fload_sk(), msg))
-        if sys.argv[-1] == '-h': sgn += '\n' + msg
-        output(sgn)
+        if sys.argv[-1] == '-h':
+            sgn += '\n' + msg
+            output(sgn, '')
+        else:            
+            output(sgn)
     elif arg1 == 'verify':
-        if invalid_sig(fload_vk(), load_sig(), load_msg()):
-            print("NOT VALID")
+        try:
+            if invalid_sig(fload_vk(), load_sig(), load_msg()):
+                print("NOT VALID: MISMATCH")
+                exit(1)
+        except SystemExit:
+            raise
+        except:
+            print("NOT VALID: FORMAT ERROR")
             exit(1)
     else:
         return usage()
 
 if __name__ == '__main__': cli()
+
