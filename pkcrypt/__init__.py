ce4185610065d0dfa088f0c36f67bfd75a6567a4
Joel Ward <jmward@gmail.com>
2018-03-22 21:47:45 -0700

    update

--- pkcrypt/__init__.py
+++ pkcrypt/__init__.py
@@ -2,7 +2,7 @@
 pkcrypt - public key cryptography
 """
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 
 from pkcrypt.api import *
 from pkcrypt.cli import *
--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
-
-from distutils.core import setup
-
+import sys
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 setup(name='pkcrypt',
-      version='0.1.0',
+      version='1.0.0',
       description='public key cryptography',
       author='Joel Ward',
       author_email='jmward@gmail.com',
+      license='MIT',
+      platforms='any',
       url='https://github.com/val-labs/pkcrypt/tree/master/pkcrypt',
       packages=['pkcrypt'],
       scripts=['bin/pkcrypt','bin/pkdcrypt','bin/pkecrypt'],
+      install_requires=['fastecdsa','pyaml'],
      )
